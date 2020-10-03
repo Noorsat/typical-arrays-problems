1ea80aad9550724e25dbc101b0b9357a1311e32c
itsnoorsat <70801140+Noorsat@users.noreply.github.com>
2020-10-03 21:41:07 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-exports.min = function min (array) {
+exports.min = function min(array) {
+	 if (array == null || array.length == 0){ return 0;}
     return Math.min.apply(Math,array);
   }
   
-  exports.max = function max (array) {
-    return Math.min.apply(Math,array);
+  exports.max = function max(array) {
+  	 if (array == null || array.length == 0) { return 0;}
+    return Math.max.apply(Math,array);
   }
   
-  exports.avg = function avg (array) {
+  exports.avg = function avg(array) {
+  	if (array == null || array.length == 0) { return 0;}
     let sum = 0;
     for (let i = 0; i < array.length; i++){
         sum += array[i];
     }
-    return sum / array.length;
+    return sum / array.length;	
   }
